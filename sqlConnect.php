2d68e2d5caf491c9188e0c100b58ff8eff2631fb
Alexy Da Silva <alexy.dasilva@icloud.com>
2022-06-05 15:46:31 +0200

    correctif

--- sqlConnect.php
+++ sqlConnect.php
@@ -3,7 +3,7 @@
     {
     // Souvent on identifie cet objet par la variable $conn ou $db
     $mysqlClient = new PDO(
-    'mysql: host=localhost; dbname=xmktwers_alexybd; charset=utf8', 'root');
+    'mysql: host=localhost; dbname=xmktwers_alexybd; charset=utf8', 'alexybd', 'b82vivS@iiXS68^cyb');
     }
     catch (Exception $e)
     {
