581a9f169f3e971224f4f0df65613edb51d17f30
Alexy Da Silva <alexy.dasilva@icloud.com>
2022-06-05 19:31:05 +0200

    correctif

--- sqlConnect.php
+++ sqlConnect.php
@@ -3,7 +3,7 @@
     {
     // Souvent on identifie cet objet par la variable $conn ou $db
     $mysqlClient = new PDO(
-    'mysql: host=localhost; dbname=overwatchsite; charset=utf8', 'root');
+    'mysql: host=localhost; dbname=xmktwers_alexybdd; charset=utf8', 'xmktwers_alexy', 'FJM~N#8N+rad7!d~a8');
     }
     catch (Exception $e)
     {
